176fdd53fd4c24fcede858de739d87da329e2d45
FastInflater Dev <fastinflater@dev.local>
2026-05-21 14:40:03 +0800

    build: 配置 maven-publish 发布到 GitHub Packages
    - 根 build.gradle.kts 统一管理 libVersion 和 libGroup
    - lib 模块配置 maven-publish 插件，附带 POM 信息和 sources jar
    - 通过 GITHUB_ACTOR/GITHUB_TOKEN 环境变量认证

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,8 @@ plugins {
     id("com.android.application") version "9.2.1" apply false
     id("org.jetbrains.kotlin.jvm") version "2.2.21" apply false
 }
+
+ext {
+    set("libVersion", "0.1.0")
+    set("libGroup", "com.github.donglua")
+}
--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,6 +1,13 @@
 plugins {
     id("com.android.library")
+    `maven-publish`
 }
+
+val libVersion: String by rootProject.extra
+val libGroup: String by rootProject.extra
+
+group = libGroup
+version = libVersion
 
 android {
     namespace = "com.aspect.fastinflater"
@@ -8,6 +15,7 @@ android {
 
     defaultConfig {
         minSdk = 24
+        consumerProguardFiles("consumer-rules.pro")
     }
 
     compileOptions {
@@ -15,6 +23,11 @@ android {
         targetCompatibility = JavaVersion.VERSION_11
     }
 
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+        }
+    }
 }
 
 dependencies {
@@ -22,4 +35,47 @@ dependencies {
     implementation("androidx.annotation:annotation:1.7.1")
     compileOnly("androidx.databinding:databinding-runtime:8.2.0")
     compileOnly("androidx.recyclerview:recyclerview:1.3.2")
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+                groupId = libGroup
+                artifactId = "fast-inflater"
+                version = libVersion
+
+                pom {
+                    name.set("FastInflater")
+                    description.set("High-performance Android LayoutInflater with view pooling, async inflate, and adaptive tuning.")
+                    url.set("https://github.com/donglua/FastInflater")
+                    licenses {
+                        license {
+                            name.set("The Apache License, Version 2.0")
+                            url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                        }
+                    }
+                    scm {
+                        connection.set("scm:git:https://github.com/donglua/FastInflater.git")
+                        developerConnection.set("scm:git:ssh://github.com/donglua/FastInflater.git")
+                        url.set("https://github.com/donglua/FastInflater")
+                    }
+                }
+            }
+        }
+
+        repositories {
+            maven {
+                name = "GitHubPackages"
+                url = uri("https://maven.pkg.github.com/donglua/FastInflater")
+                credentials {
+                    username = System.getenv("GITHUB_ACTOR")
+                        ?: project.findProperty("gpr.user") as String?
+                    password = System.getenv("GITHUB_TOKEN")
+                        ?: project.findProperty("gpr.token") as String?
+                }
+            }
+        }
+    }
 }
